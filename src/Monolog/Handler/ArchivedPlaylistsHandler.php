ac6ff9fc57767fdd726360d0c86d1df7ff270ea2
stevenfoncken <dev@stevenfoncken.de>
2023-11-15 18:28:08 +0100

    add monolog

---
+++ src/Monolog/Handler/ArchivedPlaylistsHandler.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace StevenFoncken\MultiToolForSpotify\Monolog\Handler;
+
+use Monolog\LogRecord;
+use Monolog\Handler\HandlerWrapper;
+
+/**
+ * A HandlerWrapper that adds custom filtering so that only archive playlist creation is logged.
+ * It can be applied to other handlers e.g. StreamHandler.
+ *
+ * @author Steven Foncken <dev@stevenfoncken.de>
+ * @copyright ^
+ * @license https://github.com/stevenfoncken/multitool-for-spotify-php/blob/master/LICENSE - MIT License
+ */
+class ArchivedPlaylistsHandler extends HandlerWrapper
+{
+    /**
+     * @param LogRecord $record
+     *
+     * @return bool
+     */
+    public function handle(LogRecord $record): bool
+    {
+        if ($record->message !== 'archivePlaylist: Done') {
+            return false;
+        }
+
+
+        return $this->handler->handle($record);
+    }
+}
