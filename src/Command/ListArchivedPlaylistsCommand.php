e8b979b0f9650bda08773af10ee3a64f2ec8d8a4
stevenfoncken <dev@stevenfoncken.de>
2023-12-29 11:48:15 +0100

    add ListArchivedPlaylistsCommand

---
+++ src/Command/ListArchivedPlaylistsCommand.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace StevenFoncken\MultiToolForSpotify\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Helper\Table;
+use StevenFoncken\MultiToolForSpotify\Service\PlaylistService;
+
+/**
+ * Console command that lists archived playlists.
+ *
+ * @author Steven Foncken <dev@stevenfoncken.de>
+ * @copyright ^
+ * @license https://github.com/stevenfoncken/multitool-for-spotify-php/blob/master/LICENSE - MIT License
+ */
+#[AsCommand(
+    name: 'mtfsp:archive:list-playlists',
+    description: 'List archived playlists',
+)]
+class ListArchivedPlaylistsCommand extends Command
+{
+    /**
+     * @param PlaylistService $playlistService
+     */
+    public function __construct(
+        private readonly PlaylistService $playlistService
+    ) {
+        parent::__construct();
+    }
+
+    /**
+     * @param InputInterface  $input
+     * @param OutputInterface $output
+     *
+     * @return int
+     */
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $table = new Table($output);
+        $table->setHeaders(['Name', 'URL']);
+
+        foreach ($this->playlistService->findAllArchivedPlaylists() as $playlist) {
+            $table->addRow([$playlist->name, $playlist->external_urls->spotify]);
+        }
+
+        $table->render();
+
+
+        return Command::SUCCESS;
+    }
+}
