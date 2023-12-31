e20705240fb278dbbccbf829ab067afdcfc6e43e
stevenfoncken <dev@stevenfoncken.de>
2023-12-31 13:53:08 +0100

    add DeleteArchivedPlaylistsCommand

---
+++ src/Command/DeleteArchivedPlaylistsCommand.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace StevenFoncken\MultiToolForSpotify\Command;
+
+use Psr\Log\LoggerInterface;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Helper\Table;
+use Symfony\Component\Console\Formatter\OutputFormatterStyle;
+use StevenFoncken\MultiToolForSpotify\Service\PlaylistService;
+use StevenFoncken\MultiToolForSpotify\Console\Style\CustomStyle;
+
+/**
+ * Console command that deletes archived playlists.
+ *
+ * @author Steven Foncken <dev@stevenfoncken.de>
+ * @copyright ^
+ * @license https://github.com/stevenfoncken/multitool-for-spotify-php/blob/master/LICENSE - MIT License
+ */
+#[AsCommand(
+    name: 'mtfsp:archive:delete-playlists',
+    description: 'Delete archived playlists',
+)]
+class DeleteArchivedPlaylistsCommand extends Command
+{
+    /**
+     * @param LoggerInterface $logger
+     * @param PlaylistService $playlistService
+     */
+    public function __construct(
+        private LoggerInterface $logger,
+        private readonly PlaylistService $playlistService
+    ) {
+        parent::__construct();
+    }
+
+    /**
+     * @param InputInterface  $input
+     * @param OutputInterface $output
+     *
+     * @return int
+     */
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $this->logger->info('Archived playlist deletion process: Start');
+
+        $section = $output->section();
+        $io = new CustomStyle($input, $section);
+
+        // Switch 'ask' color to magenta
+        $oldOutputFormatterStyle = $io->getFormatter()->getStyle('info');
+        $outputStyleRed = new OutputFormatterStyle('red');
+        $io->getFormatter()->setStyle('info', $outputStyleRed);
+
+        // ---
+
+        $confirmation = $io->confirm('DO YOU WANT TO DELETE ALL ARCHIVED PLAYLISTS?', false);
+        if ($confirmation === false) {
+            return Command::SUCCESS;
+        }
+        $section->clear();
+
+        $confirmation = $io->confirm('Are you really sure?');
+        if ($confirmation === false) {
+            return Command::SUCCESS;
+        }
+        $section->clear();
+
+        // ---
+
+        // Set OutputFormatterStyle to initial
+        $io->getFormatter()->setStyle('info', $oldOutputFormatterStyle);
+
+        $table = new Table($section);
+        $table->setHeaders(['Deleted playlists']);
+
+        foreach ($this->playlistService->deleteAllArchivedPlaylists() as $deletedPlaylist) {
+            $table->appendRow([$deletedPlaylist->name]);
+        }
+
+        $this->logger->info('Archived playlist deletion process: Done');
+
+
+        return Command::SUCCESS;
+    }
+}
